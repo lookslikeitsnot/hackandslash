49d4704e664aef1d56a47759fcd33bf8f4090b97
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-03-29 12:22:37 +0200

    with HUD background

--- src/main/java/be/kiop/UI/HUD.java
+++ src/main/java/be/kiop/UI/HUD.java
@@ -1,25 +1,43 @@
 package be.kiop.UI;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import be.kiop.characters.ennemies.Ennemy;
 import be.kiop.characters.heroes.Hero;
+import be.kiop.exceptions.SkinNotFoundException;
 
 public class HUD extends JPanel{
 	private static final long serialVersionUID = 1L;
+	private BufferedImage backgroundImage;
 
 //	private Hero hero;
 //	private Ennemy ennemy;
 
 	public HUD(Hero hero, Ennemy ennemy) {
+		try {
+			backgroundImage = ImageIO.read(new File("src/main/resources/images/backgrounds/background.png"));
+		} catch (IOException e) {
+			backgroundImage = null;
+			throw new SkinNotFoundException();
+		}
 		setLayout(new BorderLayout());
 		JPanel heroStatPanel = new StatPanel(hero);
 		add(heroStatPanel, BorderLayout.WEST);
 		if (ennemy != null) {
 			JPanel ennemyStatPanel = new StatPanel(ennemy);
-			add(ennemyStatPanel, BorderLayout.WEST);
+			add(ennemyStatPanel, BorderLayout.EAST);
 		}
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawImage(backgroundImage, 0,0, this.getWidth(), this.getHeight(), null);
 	}
 }
--- src/main/java/be/kiop/UI/StatPanel.java
+++ src/main/java/be/kiop/UI/StatPanel.java
@@ -1,9 +1,12 @@
 package be.kiop.UI;
 
+import java.awt.Color;
+
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.UIManager;
 
 import be.kiop.characters.GameCharacter;
 import be.kiop.events.HealthEvent;
@@ -13,18 +16,32 @@ public class StatPanel extends JPanel implements HealthListener{
 	private static final long serialVersionUID = 1L;
 	
 	private JProgressBar healthBar;
+	private JLabel nameLabel;
+	private JLabel healthLabel;
+	
 	private GameCharacter gc;
 	
 	public StatPanel(GameCharacter gc) {
+		UIManager.put("ProgressBar.background", Color.RED);
+		UIManager.put("ProgressBar.foreground", Color.GREEN);
+		UIManager.put("ProgressBar.selectionBackground", Color.BLACK);
+		UIManager.put("ProgressBar.selectionForeground", Color.BLACK);
+		
 		this.gc = gc;
 		this.gc.addHealthListener(this);
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		add(new JLabel(gc.getName()));
-		add(new JLabel("Health"));
+		
+		nameLabel = new JLabel(gc.getName());
+		healthLabel = new JLabel("Health");
+		nameLabel.setForeground(Color.red);
+		healthLabel.setForeground(Color.red);
+		add(nameLabel);
+		add(healthLabel);
 		healthBar = new JProgressBar(0, (int) gc.getMaxHealth());
 		healthBar.setValue((int) gc.getHealth());
 		healthBar.setStringPainted(true);
 		healthBar.setString((int)gc.getHealth() + "/" + (int)gc.getMaxHealth());
+		setOpaque(false);
 		add(healthBar);
 	}
 	
@@ -32,6 +49,5 @@ public class StatPanel extends JPanel implements HealthListener{
 	public void healthChanged(HealthEvent event) {
 		healthBar.setValue((int) gc.getHealth());
 		healthBar.setString((int)gc.getHealth() + "/" + (int)gc.getMaxHealth());
-		System.out.println("cou");
 	}
 }
